ea060c284fc88700d0e393d743a6d94ffb233d23
akif98 <72723783+akif98@users.noreply.github.com>
2022-10-08 09:19:03 +0800

    Add files via upload

---
+++ input_function.py
@@ -0,0 +1,18 @@
+#Input function
+name = input("Enter Your Name:")
+surname = input ("Enter your Surname:")
+print ("Your name:", name)
+print ("Your surname:", surname)
+print ("Your full name:", name,surname)
+
+
+a=int(input("Enter your first number to add:"))
+b=int(input("Enter your second number to add:"))
+c=(a+b)
+print("Addition of numbers are:",c)
+
+
+a=float(input("Enter your first number to add:"))
+b=float(input("Enter your second number to add:"))
+c=(a+b)
+print("Addition of numbers are:",c)
