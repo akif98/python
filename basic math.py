fc31215a39bbf349e78fbb3de6e0a5293d99ce4e
akif98 <72723783+akif98@users.noreply.github.com>
2022-10-09 09:16:54 +0800

    Add files via upload

---
+++ basic math.py
@@ -0,0 +1,29 @@
+#Addition program
+a = 79
+b = 21
+c = a+b
+print ("The sum of a+b is", c)
+
+#Subtraction program
+a=81
+b=27
+c=a-b
+print ("Subtraction of a-b is", c)
+
+#Multiplication program
+a=7
+b=15
+c=a*b
+print ("Multiplication of a*b is",c)
+
+#Division program
+a=15
+b=3
+c=a/b
+print("Division of a/b is",c)
+
+#Modulas program
+a=4
+b=3
+c=a%b
+print("Remainder of a%b is", c)
